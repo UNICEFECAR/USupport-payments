7dfc196dbb71d73302b2833cd79492ec1222928b
Sebastian Galustyan <sebastian@7digit.io>
2023-01-22 22:32:46 +0000

    Create: getConsultationByIdQuery to get information about existent consultations.

---
+++ service/queries/consultations.js
@@ -0,0 +1,18 @@
+import { getDBPool } from "#utils/dbConfig";
+
+export const getConsultationByIdQuery = async ({
+  poolCountry,
+  consultationId,
+}) =>
+  await getDBPool("clinicalDb", poolCountry).query(
+    `
+  
+        SELECT price 
+        FROM consultation
+        WHERE consultation_id = $1
+        ORDER BY created_at DESC
+        LIMIT 1;
+  
+      `,
+    [consultationId]
+  );
