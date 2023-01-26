a3f82b65ad4c5ab43f9393583c4357c3f5caa629
Sebastian Galustyan <sebastian@7digit.io>
2023-01-26 23:40:20 +0000

    Add: getTrasanctionByConsultationIdQuery and add new paymentRefundId param to addTransactionQuery

--- service/queries/transactions.js
+++ service/queries/transactions.js
@@ -5,14 +5,28 @@ export const addTransactionQuery = async ({
   type,
   consultationId,
   paymentIntent,
+  paymentRefundId,
 }) =>
   await getDBPool("clinicalDb", poolCountry).query(
     `
 
-        INSERT INTO transaction_log (type, consultation_id, payment_intent)
-        VALUES ($1, $2, $3)
+        INSERT INTO transaction_log (type, consultation_id, payment_intent, payment_refund_id)
+        VALUES ($1, $2, $3, $4)
         RETURNING *;
   
       `,
-    [type, consultationId, paymentIntent]
+    [type, consultationId, paymentIntent, paymentRefundId]
+  );
+
+export const getTrasanctionByConsultationIdQuery = async ({
+  poolCountry,
+  consultationId,
+}) =>
+  await getDBPool("clinicalDb", poolCountry).query(
+    `
+
+          SELECT * FROM transaction_log WHERE consultation_id = $1;
+    
+        `,
+    [consultationId]
   );
