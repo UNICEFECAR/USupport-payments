9adbe3e25211f7fcaccebd054cfcb378ad6219ff
Georgi 7DIGIT <georgi@7digit.io>
2023-07-24 14:32:10 +0300

    Fix: syntax error in getProviderNameQuery

--- service/queries/providers.js
+++ service/queries/providers.js
@@ -3,7 +3,7 @@ import { getDBPool } from "#utils/dbConfig";
 export const getProviderNameQuery = async ({ poolCountry, providerId }) =>
   await getDBPool("piiDb", poolCountry).query(
     `
-      SELECT "name", patronym, surname,
+      SELECT "name", patronym, surname
       FROM provider_detail
       WHERE provider_detail.provider_detail_id = $1
       ORDER BY provider_detail.created_at DESC
